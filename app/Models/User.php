52fdf480289cc223a821389c13d90692f2be897f
Carlos Maldonado <kardaniel1998@gmail.com>
2021-01-06 17:34:15 -0500

    Modelo y migracion para movie, genre, user

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,26 +18,26 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
-        'password',
+        'lastname',
+        'document',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+//    /**
+//     * The attributes that should be hidden for arrays.
+//     *
+//     * @var array
+//     */
+//    protected $hidden = [
+//        'password',
+//        'remember_token',
+//    ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+//    /**
+//     * The attributes that should be cast to native types.
+//     *
+//     * @var array
+//     */
+//    protected $casts = [
+//        'email_verified_at' => 'datetime',
+//    ];
 }
